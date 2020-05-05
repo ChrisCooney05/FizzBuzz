edaa3372ad187f2fd6262749f156e8ed5b8039cf
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-05-05 15:30:49 +0100

    improved fizzbuzz code

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,7 @@
+def fizzbuzz(num)
+  if num % 3 == 0
+    return "fizz"
+  elsif num % 5 == 0
+    return "buzz"
+  end
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,13 @@
 require 'fizzbuzz'
 
 describe "fizzbuzz" do
-  it "returns 'Fizz' when passed 3" do
-    expect(fizzbuz(3)).to eq "fizz"
+  it "returns 'Fizz' when passed multiple of 3" do
+    expect(fizzbuzz(3)).to eq "fizz"
+    expect(fizzbuzz(6)).to eq "fizz"
+  end
+
+  it "returns 'Buzz' when passed multiple of 5" do
+    expect(fizzbuzz(5)).to eq "buzz"
+    expect(fizzbuzz(10)).to eq "buzz"
   end
 end
