07e70de7a45bcb654aeb482c64f0b2096f55650e
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-05-05 15:47:37 +0100

    Added Fizzbuzz rspec + code

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,12 +4,18 @@ describe "fizzbuzz" do
   it "returns 'Fizz' when passed multiple of 3" do
     expect(fizzbuzz(3)).to eq "fizz"
     expect(fizzbuzz(6)).to eq "fizz"
+    expect(fizzbuzz(7)).to eq 7
   end
 
   it "returns 'Buzz' when passed multiple of 5" do
     expect(fizzbuzz(5)).to eq "buzz"
     expect(fizzbuzz(10)).to eq "buzz"
+    expect(fizzbuzz(14)).to eq 14
   end
 
+  it "returns 'FizzBuzz' when passed mutiple of 3 and 5" do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+    expect(fizzbuzz(30)).to eq "fizzbuzz"
+  end
 
 end
